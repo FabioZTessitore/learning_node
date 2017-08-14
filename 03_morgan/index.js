ba2dd8097b16f58017aa15f97b91480a8641dc6c
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 19:11:43 +0200

    morgan

---
+++ 03_morgan/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+// morgan logger
+app.use( logger('short') );
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log('server listening on port ' + PORT);
+});
