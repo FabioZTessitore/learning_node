557d029648f443911f2a8484231a40977ac39e3f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 20:22:32 +0200

    ejs template

---
+++ 08_ejs_template/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    message: 'Hi All!',
+  });
+});
+
+app.use( (req, res) => {
+  res.statusCode = 404;
+  res.end('404!');
+});
+
+app.listen(PORT, () => {
+  console.log('server listening on port ' + PORT);
+});
