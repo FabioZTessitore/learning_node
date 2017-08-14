bf0e239689ee06e384d67ff2a6a84a20a8b9474f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 19:06:32 +0200

    middleware

---
+++ 02_middleware/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+// a simple log middleware
+app.use( (req, res, next) => {
+  console.log('request ' + req.method + ' ' + req.url + ' at ' + new Date());
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log('server listening on port ' + PORT);
+});
