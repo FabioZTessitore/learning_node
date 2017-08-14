e98cedd7985cd3e5f71d0cedd4a59ee39fdc966f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 19:36:21 +0200

    req params

---
+++ 06_req_params/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Go to /hello/<yourname>');
+});
+
+app.get('/hello/:who', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello ' + req.params.who);
+})
+
+app.use( (req, res) => {
+  res.statusCode = 404;
+  res.end('404!');
+});
+
+app.listen(PORT, () => {
+  console.log('server listening on port ' + PORT);
+});
