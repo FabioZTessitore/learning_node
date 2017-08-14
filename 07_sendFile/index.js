ba5d0ee1eb6c9c9f1c6f2f7e3b57e7b615d7b7ac
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 20:03:01 +0200

    sendFile

---
+++ 07_sendFile/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile( path.resolve(__dirname, 'public', 'index.html') );
+});
+
+app.use( (req, res) => {
+  res.statusCode = 404;
+  res.end('404!');
+});
+
+app.listen(PORT, () => {
+  console.log('server listening on port ' + PORT);
+});
